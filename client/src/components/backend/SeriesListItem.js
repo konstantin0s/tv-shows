aef40e43ba5cad37e604102ea79b72a299ed51b0
konstantin0s <constantintofan85@gmail.com>
2020-05-28 19:02:43 +0200

    install react packages

--- client/src/components/backend/SeriesListItem.js
+++ client/src/components/backend/SeriesListItem.js
@@ -1,16 +1,62 @@
 import React from 'react';
+import { Link } from "react-router-dom";
+// import './css/Gif.css';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import ReactFancyBox from 'react-fancybox';
+import 'react-fancybox/lib/fancybox.css';
 
 const SeriesListItem = (props) => {
-    
+    const classes = useStyles();
+
     console.log(props);
     return (
-        <div>
-                <li key={props.series.id}>
-                <h2 className="name">{props.series.name}</h2>
-                <img className="image" src={props.series.image.medium ? props.series.image.medium : ''} />
-                </li>
+        // <div>
+        //         <li key={props.series.id}>
+        //         <h2 className="name">{props.series.name}</h2>
+        //         <img className="image" src={props.series.image.medium ? props.series.image.medium : ''} />
+        //         </li>
+        // </div>
+        <div className="child-gif">
+        <Card className={classes.root}>
+          <CardActionArea>
+                 <ReactFancyBox className={classes.media} thumbnail={props.series.image.medium ? props.series.image.medium : ''}
+                  image={props.series.image.medium ? props.series.image.medium : ''} />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="h2">
+                {props.series.name}
+              </Typography>
+              <Typography variant="body2" color="textSecondary" component="p">
+             {props.series.type}
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+          <CardActions>
+    
+            <Link className="linkx" id={props.series.id} to={`/show/${props.series.id}`}>
+                    See More..
+          </Link>
+          
+          </CardActions>
+        </Card>
         </div>
     )
 }
 
+
 export default SeriesListItem;
+
+
+const useStyles = makeStyles({
+    root: {
+      maxWidth: 345,
+    },
+    media: {
+      height: 160,
+      width: 345
+    },
+  });
